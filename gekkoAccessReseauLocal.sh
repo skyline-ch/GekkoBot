19aa44d33684627c7136215305fa3cfc738270d0
Skyline-ch <courvoisier@netplus.ch>
2018-08-18 12:53:24 +0200

    Furst commit
    Premier commite du fichier de modification contiguration reseau pour
    Gekko

---
+++ gekkoAccessReseauLocal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script qui permet de modifier la configuration de Gekko pour
+# le redandre accèssible sur le réseau local domestique
+#
+# Version : 0.1
+# Date le 18.08.2018
+# Créer par PhunkyBob et Skyline-ch
+#
+##########################
+
+ip4=$(hostname -I)
+$ip4=$ip4
+echo "Votre IP : ${ip4// /}"
+
+sed -i "s/127\.0\.0\.1/0\.0\.0\.0/g; s/localhost/${ip4// /}/g; s/headless: false/headless: true/;" ./UIconfig.js
